a579a006ecdc169d7ab4937b042055b45cc93a28
Eduardo Gimenes <eduardo.gimenes@vert-capital.com>
2026-03-16 18:10:55 -0300

    studio61 project reestructuring

--- poc-studio61/models/silver-spaces.sql
+++
@@ -1,6 +0,0 @@
--- spaces
-SELECT 
-      id    AS space_id
-    , name  AS space_name
-FROM 
-    stech_solucoes_tecnologicas_bronze.spaces
--- poc-studio61/models/silver-users.sql
+++
@@ -1,6 +0,0 @@
--- users
-SELECT 
-      id        AS user_id
-    , username  AS user_name
-FROM 
-    stech_solucoes_tecnologicas_bronze.users
